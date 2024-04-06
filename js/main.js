a7339f76bad9b23c73f6f30895876ece75712635
Redither <slavonchik68@gmail.com>
2024-04-06 20:09:55 +0300

    some finish proto

--- js/main.js
+++ js/main.js
@@ -5,4 +5,12 @@ menu_btn.onclick = function(){
     menu_btn.classList.toggle('active');
     menu_list.classList.toggle('-active')
     return false;
-  };
+};
+
+const open_btns = document.querySelectorAll('.popup_btn-open');
+const popup = document.querySelector('.popup_wrapper');
+const close_popup = document.querySelector('.popup_btn-close')
+open_btns.forEach(element => {
+    element.onclick = function() {popup.classList.add('--active')}
+})
+close_popup.onclick = function() {popup.classList.remove('--active')}
--- js/popup.js
+++
@@ -1,7 +0,0 @@
-const open_btns = document.querySelectorAll('.popup_btn-open');
-const popup = document.querySelector('.popup_wrapper');
-const close_popup = document.querySelector('.popup_btn-close')
-open_btns.forEach(element => {
-    element.onclick = function() {popup.classList.add('--active')}
-})
-close_popup.onclick = function() {popup.classList.remove('--active')}
